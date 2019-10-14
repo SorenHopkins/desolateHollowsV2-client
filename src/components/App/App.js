e5fc4fe90450ac032e033eb3c3468328e367c206
SorenHopkins <Soren.hopkins@gmail.com>
2019-10-14 13:52:13 -0400

    Create Intro path
    Created path for the Introduction. Set up path through several
    paragraphs to the game.
    Added custom Favicon for the game.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ import SignUp from '../SignUp/SignUp'
 import SignIn from '../SignIn/SignIn'
 import SignOut from '../SignOut/SignOut'
 import ChangePassword from '../ChangePassword/ChangePassword'
+import Intro from '../Intro/Intro'
 
 class App extends Component {
   constructor () {
@@ -53,6 +54,9 @@ class App extends Component {
           )} />
           <AuthenticatedRoute user={user} path='/change-password' render={() => (
             <ChangePassword alert={this.alert} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} path='/' render={() => (
+            <Intro alert={this.alert} user={user} />
           )} />
         </main>
       </Fragment>
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Intro = () => {
+  const [intro, setIntro] = useState({
+    currentSection: 0
+  })
+
+  if (intro.currentSection === 0) {
+    return (<section key="0">
+      <h4>You stumble through the <a href="#" id="1" onClick={() => setIntro({ currentSection: 1 })}>woods.</a></h4>
+    </section>)
+  } else if (intro.currentSection === 1) {
+    return (<section key="1">
+      <p>It’s dark. You can hear the cry of the owls in the trees. In the distance a wolf howls. You know where you’re <a href="#" onClick={() => setIntro({ currentSection: 2 })}>going.</a></p>
+    </section>)
+  } else if (intro.currentSection === 2) {
+    return (<section key="2">
+      <p>Dry branches block your path, scratch your skin. Leaves crunch under your feet. Too loud.</p>
+      <p>If only the snow would fall faster. Maybe that would disguise the sound. The howls of the wolves are getting <a href="#" onClick={() => setIntro({ currentSection: 3 })}>closer.</a></p>
+    </section>)
+  } else if (intro.currentSection === 3) {
+    return (<section key="3">
+      <p>You can <a href="#" id="3" onClick={() => setIntro({ currentSection: 4 })}>smell</a> it now. </p>
+    </section>)
+  } else if (intro.currentSection === 4) {
+    return (<section key="4">
+      <p>You push past the branches into the clearing. Ahead of you: a huge tree. Old, dead, hollowed out. A misshapen <a href="#" onClick={() => setIntro({ currentSection: 5 })}>door</a> affixed to a misshapen opening.</p>
+    </section>)
+  } else if (intro.currentSection === 5) {
+    return (<section key="5">
+      <p>It doesn’t budge. You pull again. Still, nothing. You remind the door that your are a very powerful <a href="#" onClick={() => setIntro({ currentSection: 6 })}>witch</a>, and it ought to let you through. A moment. It opens.</p>
+    </section>)
+  } else if (intro.currentSection === 6) {
+    return (<section key="6">
+      <p>It’s warm. You slam the door shut in front of you. In the center of the room a cauldron bubbles, wafting a scent of pumpkin and cinnamon. The last witch must have been cooking. All along the walls, dozens of vials of <a href="#" onClick={() => setIntro({ currentSection: 7 })}>ingredients</a> are arrayed: bats wings, newts eyes, holy water…</p>
+    </section>)
+  } else if (intro.currentSection === 7) {
+    return (<section key="7">
+      <p>But no unicorn horn. Over half the ingredients are missing. The last witch wasn’t as diligent as she ought to have been. You’ll have to get to work right away. You can’t afford to leave this place a mess. But first, there’s a cot in the corner. You need some rest. You can start work <Link to="/">tomorrow.</Link></p>
+    </section>)
+  }
+}
+
+export default Intro
