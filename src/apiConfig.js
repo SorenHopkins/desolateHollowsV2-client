3cc198238503ceb45c0e8b7d0c282384e82cad8b
SorenHopkins <Soren.hopkins@gmail.com>
2019-10-17 14:46:05 -0400

    Update config to connect to correct server
    Updated apiConfig to direct to the correct Heroku server

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'http://shrouded-ridge-13366.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
